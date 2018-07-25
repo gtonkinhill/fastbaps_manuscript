323257811ea9f91739e6ac969bf809d588493bc1
Gerry Tonkin-Hill <gqt20@cam.ac.uk>
2018-07-25 13:09:01 +0100

    added in scripts

---
+++ run_rhierbaps.R
@@ -0,0 +1,16 @@
+library(rhierbaps)
+
+args <- commandArgs(trailingOnly=TRUE)
+
+fasta.file.name <- args[[1]]
+n.pops <- args[[2]]
+
+snp.matrix <- load_fasta(fasta.file.name)
+hb.results <- hierBAPS(snp.matrix, max.depth = 2, n.pops = n.pops, quiet = FALSE, n.cores = 5)
+
+pre.name <- tools::file_path_sans_ext(fasta.file.name)
+
+write.csv(hb.results$partition.df, file = paste(pre.name, "hierbaps_partition.csv", sep="_"), 
+          col.names = TRUE, row.names = FALSE, quote = FALSE)
+
+save_lml_logs(hb.results, paste(pre.name, "hierbaps_logML.txt", sep="_"))
