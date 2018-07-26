54b7e2208ef81abc84a681b3f6f9cb61aab98cc0
Gerry Tonkin-Hill <gqt20@cam.ac.uk>
2018-07-26 17:40:25 +0100

    added code for running fastbaps

--- submit_fastbaps_jobs.sh
+++ submit_fastbaps_jobs.sh
@@ -1,19 +1,19 @@
 FILE="./data/escherichia_coli/escherichia_coli_core_gene_alignment.snps.aln"
-nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE > "${FILE}_fastbaps.log" 2>&1 &
+nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_fastbaps.R $FILE > "${FILE}_fastbaps.log" 2>&1 &
 
 FILE="./data/haemophilus_influenzae/haemophilus_influenzae_core_gene_snps.aln"
-nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE > "${FILE}_fastbaps.log" 2>&1 &
+nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_fastbaps.R $FILE > "${FILE}_fastbaps.log" 2>&1 &
 
 FILE="./data/listeria_monocytogenes/listeria_monocytogenes_core_gene_snps.aln"
-nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE > "${FILE}_fastbaps.log" 2>&1	&
+nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_fastbaps.R $FILE > "${FILE}_fastbaps.log" 2>&1	&
 
 FILE="./data/neisseria_meningitidis/neisseria_meningitidis_core_gene_snps.aln"
-nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE > "${FILE}_fastbaps.log" 2>&1	&
+nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_fastbaps.R $FILE > "${FILE}_fastbaps.log" 2>&1	&
 
 FILE="./data/staphylococcus_aureus/staphylococcus_aureus_core_gene_alignment.snps.aln"
-nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE > "${FILE}_fastbaps.log" 2>&1	&
+nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_fastbaps.R $FILE > "${FILE}_fastbaps.log" 2>&1	&
 
 FILE="./data/ebola/Makona_1610_cds_ig.fas"
-nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE > "${FILE}_fastbaps.log" 2>&1 &
+nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_fastbaps.R $FILE > "${FILE}_fastbaps.log" 2>&1 &
 
 
