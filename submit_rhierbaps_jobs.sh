f6a10a9950f8d99eed7445dfc4497ae2f425f207
Gerry Tonkin-Hill <gqt20@cam.ac.uk>
2018-10-11 16:32:00 +0100

    updated baps to only run one level for timings

--- submit_rhierbaps_jobs.sh
+++ submit_rhierbaps_jobs.sh
@@ -1,31 +1,24 @@
 FILE="./data/escherichia_coli/escherichia_coli_core_gene_alignment.snps.aln"
 NPOP=80
-nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE $NPOP > "${FILE}.log" 2>&1 &
+nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE $NPOP > "${FILE}_rhierbaps.log" 2>&1 &
 
 FILE="./data/haemophilus_influenzae/haemophilus_influenzae_core_gene_snps.aln"
 NPOP=40
-nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE $NPOP > "${FILE}.log" 2>&1 &
+nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE $NPOP > "${FILE}_rhierbaps.log" 2>&1 &
 
 FILE="./data/listeria_monocytogenes/listeria_monocytogenes_core_gene_snps.aln"
 NPOP=40
-nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE $NPOP > "${FILE}.log" 2>&1	&
+nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE $NPOP > "${FILE}_rhierbaps.log" 2>&1	&
 
 FILE="./data/neisseria_meningitidis/neisseria_meningitidis_core_gene_snps.aln"
 NPOP=60
-nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE $NPOP > "${FILE}.log" 2>&1	&
+nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE $NPOP > "${FILE}_rhierbaps.log" 2>&1	&
 
 FILE="./data/staphylococcus_aureus/staphylococcus_aureus_core_gene_alignment.snps.aln"
 NPOP=40
-nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE $NPOP > "${FILE}.log" 2>&1	&
+nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE $NPOP > "${FILE}_rhierbaps.log" 2>&1	&
 
 FILE="./data/ebola/Makona_1610_cds_ig.fas"
 NPOP=150
-nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE $NPOP > "${FILE}.log" 2>&1 &
-
-FILE="./data/maela/maela3k_snps.fasta"
-NPOP=200
-nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE $NPOP > "${FILE}.log" 2>&1 &
-
-
-
+nohup /usr/bin/time -v Rscript --vanilla ./fastbaps_manuscript/run_rhierbaps.R $FILE $NPOP > "${FILE}_rhierbaps.log" 2>&1 &
 
