2ae533580e3c299d994b67c430a46b757587e80c
Gerry Tonkin-Hill <gqt20@cam.ac.uk>
2019-03-06 17:33:29 +0000

    added more runs

--- scripts/HIV_run_baps_prior_diff_sizes.R
+++ scripts/HIV_run_baps_prior_diff_sizes.R
@@ -1,5 +1,6 @@
 library(fastbaps)
 library(Matrix)
+library(ape)
 
 #dont time pre-processing phylogeny ext. In practive this takes very little time anyway
 hiv.data <- import_fasta_sparse_nt("./data/HIV/hiv_refs_prrt_trim.fas", prior = "baps")
@@ -9,17 +10,17 @@ cs <- colSums(hiv.data$snp.matrix>0)
 hiv.data$snp.matrix <- hiv.data$snp.matrix[,cs<200]
 
 temp.data <- hiv.data
-temp.data$snp.matrix <- temp.data$snp.matrix[sample(1:nrow(temp.data$snp.matrix), 1000)]
+temp.data$snp.matrix <- temp.data$snp.matrix[sample(1:nrow(temp.data$snp.matrix), 1000),]
 temp.data <- optimise_prior(temp.data, type = 'baps')
 system.time({hiv.multi <- multi_res_baps(temp.data, levels = 1, n.cores = 10)})
 
 temp.data <- hiv.data
-temp.data$snp.matrix <- temp.data$snp.matrix[sample(1:nrow(temp.data$snp.matrix), 10000)]
+temp.data$snp.matrix <- temp.data$snp.matrix[sample(1:nrow(temp.data$snp.matrix), 10000),]
 temp.data <- optimise_prior(temp.data, type = 'baps')
 system.time({hiv.multi <- multi_res_baps(temp.data, levels = 1, n.cores = 10)})
 
 temp.data <- hiv.data
-temp.data$snp.matrix <- temp.data$snp.matrix[sample(1:nrow(temp.data$snp.matrix), 100000)]
+temp.data$snp.matrix <- temp.data$snp.matrix[sample(1:nrow(temp.data$snp.matrix), 100000),]
 temp.data <- optimise_prior(temp.data, type = 'baps')
 system.time({hiv.multi <- multi_res_baps(temp.data, levels = 1, n.cores = 10)})
 
